382d171c9b70d13767de35c8f971a8f6c0a1a34e
saloou-mac <saloou@saloou-macdeMacBook-Air.local>
2017-12-15 20:50:31 +0800

    yingshanhong

--- config/app.php
+++ config/app.php
@@ -171,6 +171,8 @@ return [
          * Package Service Providers...
          */
 
+        Laravel\Passport\PassportServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
